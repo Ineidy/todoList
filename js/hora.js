b70fa7f1f507ad7d9078cb4679991d5491886255
Ineidy <160775201+Ineidy@users.noreply.github.com>
2024-06-23 15:08:07 -0500

    feat: :sparkles: hora y fecha
    se agregaros kas funciones de fecha y hora, y se le dio su modificacion en css

--- js/hora.js
+++ js/hora.js
@@ -1,11 +1,8 @@
-function showTime(){
-    myDate = new Date();
-    hours = myDate.getHours();
-    minutes = myDate.getMinutes();
-    seconds = myDate.getSeconds();
-    if (hours < 10) hours = 0 + hours;
-    if (minutes < 10) minutes = "0" + minutes;
-    if (seconds < 10) seconds = "0" + seconds;
-    $("#HoraActual").text(hours+ ":" +minutes+ ":" +seconds);
-    setTimeout("showTime()", 1000);
-    }
+function displayTime() {
+    const date = new Date();
+    const horas = String(date.getHours()).padStart(2, '0');
+    const minutos = String(date.getMinutes()).padStart(2, '0');
+    const segundos = String(date.getSeconds()).padStart(2, '0');
+    document.querySelector("#horaactual").innerHTML = `${horas}:${minutos}:${segundos}`;
+}
+setInterval(displayTime, 1000);
--- js/time.js
+++ js/time.js
@@ -2,4 +2,4 @@ date = new Date ();
 year = date.getFullYear();
 month = date.getMonth()+1;
 day = date.getDate();
-document.querySelector("#current_date").innerHTML = month +"/"+ day +("/")+year;
+document.querySelector("#current_date").innerHTML = month +"/"+ day +("/")+year+ (", ");
